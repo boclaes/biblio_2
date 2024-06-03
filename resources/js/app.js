4f66805a480a46ae8c46176277253744cac091e8
Myrthe Diepeveen <74008279+myrthedpvn@users.noreply.github.com>
2024-06-03 21:01:29 +0200

    js toegevoegd

--- resources/js/app.js
+++ resources/js/app.js
@@ -48,3 +48,17 @@ document.addEventListener('DOMContentLoaded', function () {
       button.addEventListener('click', () => toggleSlide(button.textContent.toLowerCase()));
   });
 });
+
+document.querySelectorAll('.accordion-button').forEach(button => {
+  button.addEventListener('click', () => {
+      const accordionContent = button.nextElementSibling;
+
+      button.classList.toggle('active');
+
+      if (button.classList.contains('active')) {
+          accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+      } else {
+          accordionContent.style.maxHeight = 0;
+      }
+  });
+});
