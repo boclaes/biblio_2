b81a22b4be3b7c10056ec0454d533055568a01aa
Myrthe Diepeveen <74008279+myrthedpvn@users.noreply.github.com>
2024-06-03 22:54:13 +0200

    oke

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
+
 // Password Reset Routes
 Route::get('forgot-password', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
 Route::post('forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
