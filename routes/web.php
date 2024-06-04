b40049ae64a96cd90827629459d6d37eb4be5b20
Myrthe Diepeveen <74008279+myrthedpvn@users.noreply.github.com>
2024-06-04 22:01:45 +0200

    nieuwe route

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::get('/support_platform', function () {
 Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
+//
 
 Route::get('/contact_platform', function () {
     return view('contact_platform');
